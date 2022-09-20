e9bda77814795b0535cd282242ad468ceb96fdf2
Esdras <esdrastmendes@gmail.com>
2022-09-19 22:33:33 -0300

    feat: função que lê o arquivo no formato csv

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+    with open(path, mode='r') as file:
+        jobs = csv.DictReader(file)
+        return list(jobs)
     return []
